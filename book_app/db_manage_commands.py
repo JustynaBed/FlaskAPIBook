4594a58ea3dc1bf6d0a4e9864a66b253887c8e84
Justyna Konieczna <justyna.bednarko@gmail.com>
2025-10-07 22:20:37 +0200

    add add_data and remove_data function to db_manage_commands.py

--- book_app/db_manage_commands.py
+++ book_app/db_manage_commands.py
@@ -1,4 +1,10 @@
+import json
+from pathlib import Path
+from sqlalchemy.sql import text
+from datetime import datetime
+
 from book_app import app, db
+from book_app.models import Author
 
 @app.cli.group()
 def db_manage():
@@ -6,11 +12,29 @@ def db_manage():
     pass
 
 @db_manage.command()
-def add_date():
+def add_data():
     """Add sample data to database"""
-    pass
+    try:
+        authors_path = Path(__file__).parent / 'samples' / 'authors.json'
+        with open(authors_path) as file:
+            data_json = json.load(file)
+        for item in data_json:
+            item['birth_date'] = datetime.strptime(item['birth_date'], '%d-%m-%Y').date()
+            author = Author(**item)
+            db.session.add(author)
+        db.session.commit()
+        print('Data has been successfully added to database')
+    except Exception as exc:
+        print('Unexpected error: {}'.format(exc))
+
 
 @db_manage.command()
-def remove_date():
+def remove_data():
     """Remove sample data to database"""
-    pass
+    try:
+        query = text('TRUNCATE authors')
+        db.session.execute(query)
+        db.session.commit()
+        print('Data has been successfully remove from database')
+    except Exception as exc:
+        print('Unexpected error: {}'.format(exc))
