d6a83945927f00f59c8778ae14a2472c0779ecaf
Justyna Konieczna <justyna.bednarko@gmail.com>
2025-10-15 14:34:11 +0200

    revert vue file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,44 +1,43 @@
 <script setup>
-// import { ref, onMounted } from "vue"
+import { ref, onMounted } from "vue"
 
-// const authors = ref([])
-// const message = ref("")
-// const count = ref(0)
+const authors = ref([])
+const message = ref("")
+const count = ref(0)
 
-// onMounted(() => {
-//   // fetchAuthors()
-// })
+onMounted(() => {
+  fetchAuthors()
+})
 
-// function fetchAuthors() {
-//   console.log('Fetch')
-//   fetch('/api/v1/authors', {
-//     method: 'GET',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log('data:', data)
-//     authors.value = data.data
-//     count.value = data.number_of_records
-//     message.value = data.success ? 'Lista autorów' : 'Błąd podczas pobierania danych'
-//   })
-//   .catch(error => {
-//     console.error('Fetch error:', error)
-//     message.value = 'Błąd połączenia z serwerem'
-//   })
-// }
+function fetchAuthors() {
+  console.log('Fetch')
+  fetch('/api/v1/authors', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('data:', data)
+    authors.value = data.data
+    count.value = data.number_of_records
+    message.value = data.success ? 'Lista autorów' : 'Błąd podczas pobierania danych'
+  })
+  .catch(error => {
+    console.error('Fetch error:', error)
+    message.value = 'Błąd połączenia z serwerem'
+  })
+}
 </script>
 
 <template>
-  <p>Vue3</p>
-  <!-- <h1>{{ message }}</h1> -->
-  <!-- <p>Liczba autorów: {{ count }}</p>
+  <h1>{{ message }}</h1>
+  <p>Liczba autorów: {{ count }}</p>
 
   <ul>
     <li v-for="author in authors" :key="author.id">
-      {{ author.name }}
+      {{ author.first_name }}
     </li>
-  </ul> -->
+  </ul>
 </template> 
